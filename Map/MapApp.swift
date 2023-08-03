30784972893b0c1ad3ec3d9ce24f682813b90e72
saj panchal <sajpanchal2020@outlook.com>
2023-08-03 11:33:25 -0400

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by saj panchal on 2023-08-03.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
