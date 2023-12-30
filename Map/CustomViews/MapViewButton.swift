698a02deddfa56d2949fdec39422ede92266c959
saj panchal <sajpanchal2020@outlook.com>
2023-12-30 13:13:26 -0500

    MapViewButton file commented.

--- Map/CustomViews/MapViewButton.swift
+++ Map/CustomViews/MapViewButton.swift
@@ -7,40 +7,44 @@
 
 import SwiftUI
 
+///custom buttons to be used  for MapView
 struct MapViewButton: View {
+    ///systemname of the image to be displayed on the button
     var imageName: String = ""
     
     var body: some View {
-        //this is the button to start tracking the user location. when it is tapped, MapView will start tracking the user location by
-        //instantiating the mapView camera with its associated properties.
+        ///enclose the hstack in vstack with a spacer above so the button view will stay at the bottom of the screen.
         VStack {
             Spacer()
+            ///enclose the zstack in HStack with a spacer to the left so the button view will appear with right alignment.
             HStack {
                 Spacer()
+                ///Stack that adds one view on top of the other (from top to bottom it adds views above each)
                 ZStack {
+                    ///add a rectangle shape as a background view for the button
                     Rectangle()
                         .frame(width: 50, height: 50)
                         .foregroundStyle(Color.white.gradient)
                         .cornerRadius(10)
+                    ///if the image name is circle or circle with fill
                     if imageName == "circle" || imageName == "circle.fill" {
+                        ///add a circle with gray gradiant on top of a rectangle.
                         Circle()
                             .frame(width: 30, height: 30)
                             .foregroundStyle(Color.gray.gradient)
                     }
-                    
+                    ///add an image to be displayed
                     Image(systemName: imageName)
                         .resizable()
                         .scaledToFit()
                         .frame(width: 25, height: 25)
+                    ///set its color to blue gradient if image is having a circle filled otherwise set the gray geadient.
                         .foregroundStyle(imageName == "circle.fill" ? Color.blue.gradient : Color.gray.gradient)
                 }
-                
-                
             }
             .padding(10)
         }
         .padding(20)
-         
     }
 }
 
