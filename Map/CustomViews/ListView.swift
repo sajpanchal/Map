7c65ae20ba4ff5f0b4ce249a99435a4cd9497da8
saj panchal <sajpanchal2020@outlook.com>
2024-01-01 12:23:41 -0500

    commented listView file

--- Map/CustomViews/ListView.swift
+++ Map/CustomViews/ListView.swift
@@ -7,22 +7,28 @@
 
 import SwiftUI
 
+///this swiftui view is responsible for showing up the list of suggested addresses when user is typing any address string in the searchfield.
 struct ListView: View {
-   
-    
+    ///local search object that handles the search queries/
     @StateObject var localSearch: LocalSearch
+    ///bounded string that stores the searched string getting from the searchfield input
     @Binding var searchedLocationText: String
    
     var body: some View {
+        ///list view
         List {
+            ///this loop will iterate for each result in the results array of localSearch object which is unique. and from it we will display the results seperated by its title and subtitle in a VStack
             ForEach(localSearch.results, id:\.self.id) { suggestion in
+                ///enclose the hstack in vstack
                 VStack {
+                    ///display the result title
                     HStack {
                         Text("\(suggestion.title)")
                             .font(.body)
                             .fontWeight(.bold)
                         Spacer()
                     }
+                    ///display the result subtitle.
                     HStack {
                         Text(" \(suggestion.subtitle)")
                             .font(.caption)
@@ -30,17 +36,17 @@ struct ListView: View {
                         Spacer()
                     }
                 }
+                ///on tap of this list item the code in the braces will be executed to get that location in the map
                 .onTapGesture {
+                    ///this method will pin the given location in the map.
                     localSearch.getPlace(from: suggestion)
+                    ///replace the partial searched text with the complete title received from selected location
                     searchedLocationText = (searchedLocationText == suggestion.title) ? (suggestion.title + " ") : suggestion.title
+                    ///set the flag true to indicate that destination has been selected.
                     localSearch.isDestinationSelected = true
-                    print("tapped list item")
-               
                 }
             }
         }
-     
-      
     }
 }
 
